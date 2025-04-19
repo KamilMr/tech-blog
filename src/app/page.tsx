de4eb1cdbbc85371ad75505f6008296e49b347ac
kamil <kamil@chicos.life>
2025-04-19 19:21:52 +0200

    Update RSS feed with new articles and adjust layout in homepage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import { addToFeed, getPostMetadata } from './utils';
 const Home = async () => {
   const posts = getPostMetadata();
 
-  addToFeed(posts.slice(0, 10));
+  addToFeed(posts.slice(0, 20));
 
   const postPreview = posts.map((post: any) => (
     <PostPreview key={post.slug} {...post} />
   ));
-  return <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>{postPreview}</div>;
+  return <div className='grid grid-cols-1 gap-4'>{postPreview}</div>;
 };
 
 export default Home;
